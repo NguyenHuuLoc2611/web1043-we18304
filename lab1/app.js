db5491817a2760ca22a202caefe815a5318fc1d0
Nguyen Huu LocLoc <locnhpd08140@fpt.edu.vn>
2023-03-10 17:58:12 +0700

    Nop Lab1

--- lab1/app.js
+++ lab1/app.js
@@ -1,6 +1,5 @@
-console.log("Hello World");
-let a = 10;
-//set a value of 10
-alert(a);
-var x= prompt("Nhap vao gia tri n: ");//Nhap dữ liệu
-document.write(x);//xuất dữ liệu ra màn hình 
+ let a=  parseInt(prompt("Nhap vao gia tri a: "));//Nhap dữ liệu cua a
+ let b= parseInt(prompt("Nhap vao gia tri b: "));//Nhap dữ liệu cua b
+let c= a+b;//c= a+b
+document.write(c);//xuất dữ liệu ra màn hình 
+console.log(c);//xuat ket qua ra console
