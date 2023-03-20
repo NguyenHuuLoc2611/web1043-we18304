092e336c6fa637c60e7b8a5cb476dc24e161abd8
Nguyen Huu LocLoc <locnhpd08140@fpt.edu.vn>
2023-03-20 18:12:27 +0700

    NopLab2Lab22

--- lab2/bai2.js
+++ lab2/bai2.js
@@ -5,11 +5,11 @@ let c = parseInt(prompt ("Nhap canh c: "));
 //nếu tổng 2 cạnh lớn hơn cạnh còn lại thì đó là tam giác và ngược lại
 if( a<b+c && b<a+c && c<a+b ){
     if( a*a==b*b+c*c || b*b==a*a+c*c || c*c== a*a+b*b)//nếu bình phương 1 cạnh bằng tổng 2 cạnh còn lại thì đó là tam giác vuông
-    console.log("Day la tam giac vuong");
+    {console.log("Day la tam giac vuong");}
     else if(a==b || a==c || b==c)//nếu cạnh  a=canh b = canh c thì đó là tam giác cân
-    console.log("Day la tam giac can.");
+    {console.log("Day la tam giac can.");}
     else
-    console.log("Day la tam giac thuong");//và trường hợp cuối là tam giác thường 
+    {console.log("Day la tam giac thuong");}//và trường hợp cuối là tam giác thường 
     }
     else
-    console.log("Day khong phai la tam giac");
+    {console.log("Day khong phai la tam giac");}
